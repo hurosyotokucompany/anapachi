211fe46a52a0cdbc435ee573eb265bd50b5b2e55
Newnuma <newnumataku@gmail.com>
2024-02-06 22:34:03 +0900

    dss

--- Assets/Script/Play/HorrorStage/Ghost.cs
+++ Assets/Script/Play/HorrorStage/Ghost.cs
@@ -9,6 +9,9 @@ class Ghost : MonoBehaviour
     Rigidbody myRigidbody;
     float speed = 2f;
     float minSpeed = 0.1f;
+    private float timer = 0f;
+    // 拡大する間隔（秒）
+    private float interval = 5f;
 
     void Start()
     {
@@ -21,34 +24,40 @@ class Ghost : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody>();
         Vector3 rvelocity = myRigidbody.velocity;
-        float sqrt2 = Mathf.Sqrt(2);
-        // 総速度（magnitude）を計算
-        float currentSpeed = rvelocity.magnitude;
-
-        if (Mathf.Abs(rvelocity.x) < minSpeed )
-        {
-            Debug.Log(rvelocity.y+"x_0");
-            float newx = rvelocity.x + Mathf.Sign(rvelocity.x)*minSpeed/3;
-            float newy = Mathf.Sign(rvelocity.y)*Mathf.Sqrt(2*speed*speed-newx*newx);
-            myRigidbody.velocity = new Vector3(newx, newy, 0f);
-
-        }
-        else if (Mathf.Abs(rvelocity.y) < minSpeed )
-        {
-            Debug.Log(rvelocity.y+"y_0");
-            float newy = rvelocity.y + Mathf.Sign(rvelocity.y)*minSpeed;
-            float newx = Mathf.Sign(rvelocity.x)*Mathf.Sqrt(2*speed*speed-newy*newy);
-            Debug.Log(newy+"newy");
-            Debug.Log(newx+"newx");
-            myRigidbody.velocity = new Vector3(newx, newy, 0f);
-            Debug.Log(myRigidbody.velocity+"velocity");
-        }
 
         if(transform.position.x > -12 )
         {
             rvelocity.y=-1*rvelocity.y;
         }
 
+                // タイマーを更新
+        timer += Time.fixedDeltaTime;
+        // 5秒ごとにオブジェクトのスケールを3倍にする
+        if (timer >= interval)
+        {
+            // transform.localScale *= 3; // 現在のスケールを3倍にする
+            StartCoroutine(EnlargeImage());
+            timer = 0f; // タイマーをリセット
+        }
+
     
+    }
+
+    private IEnumerator EnlargeImage()
+    {
+        float duration = 0.5f;
+        float currentTime = 0f;
+        Vector3 startSize = transform.localScale; // initial scale before enlargement
+        Vector3 endSize = startSize * 3; // target scale for full screen
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float t = currentTime / duration;
+            transform.localScale = Vector3.Lerp(startSize, endSize, t);
+            yield return null;
+        }
+
+
     }
 }
