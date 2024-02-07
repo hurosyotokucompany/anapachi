5773b5addd60e30505353cbfda97632238c54cf9
hurosyotokucompany <135603294+hurosyotokucompany@users.noreply.github.com>
2024-02-07 23:33:56 +0900

    yy

--- Assets/Script/Home/RewardedAdsButton.cs
+++ Assets/Script/Home/RewardedAdsButton.cs
@@ -57,7 +57,7 @@ public class RewardedAdsButton : MonoBehaviour, IUnityAdsLoadListener, IUnityAds
         UnityEngine.Debug.Log("S");
         int rnd = Random.Range(1, 11);
         UnityEngine.Debug.Log(rnd);
-        if(rnd>=1){
+        if(rnd>=8){
         // Disable the button:
         _showAdButton.interactable = false;
         // Then show the ad:
