6e2421035bc070f573988dcbc4a1f637f352c22a
hurosyotokucompany <135603294+hurosyotokucompany@users.noreply.github.com>
2024-02-08 23:20:10 +0900

    yy

--- Assets/Script/Home/AdMobScript.cs
+++ Assets/Script/Home/AdMobScript.cs
@@ -14,16 +14,18 @@ public class AdMobScript : MonoBehaviour
   private void RequestBanner()
   {
 #if UNITY_ANDROID
-    string adUnitId = "ca-app-pub-3940256099942544/6300978111"; // テスト用広告ユニットID
+    string adUnitId="ca-app-pub-6195226537990500/5053751594";
+    //string adUnitId = "ca-app-pub-3940256099942544/6300978111"; // テスト用広告ユニットID
 #elif UNITY_IPHONE
-    string adUnitId = "ca-app-pub-3940256099942544/2934735716"; // テスト用広告ユニットID
+    string adUnitId="ca-app-pub-6195226537990500/1208478181";
+    //string adUnitId = "ca-app-pub-3940256099942544/2934735716"; // テスト用広告ユニットID
 #else
     string adUnitId = "unexpected_platform";
 #endif
     // Create a 320x50 banner at the bottom of the screen.
     AdSize customAdSize = new AdSize(320, 100);
-    //this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
-    this.bannerView = new BannerView(adUnitId,customAdSize, AdPosition.Bottom);
+    this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+    //this.bannerView = new BannerView(adUnitId,customAdSize, AdPosition.Bottom);
     // Create an empty ad request.
     AdRequest request = new AdRequest();
     // Load the banner with the request.
